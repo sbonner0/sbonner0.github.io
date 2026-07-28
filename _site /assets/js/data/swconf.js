0bf05127a9b82928b2f003f89300121e483162dd
Stephen Bonner <stephen.bonner0@icloud.com>
2026-07-28 20:09:30 +0100

    clean and fix theme

--- _site /assets/js/data/swconf.js
+++
@@ -1,1 +0,0 @@
-const swconf = { cacheName: 'chirpy-1785185247',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
